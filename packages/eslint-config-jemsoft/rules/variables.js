20204970f6ad0b80b0380cd2ad982e425a722cd0
Farbod Motlagh <farbod.motlagh@gmail.com>
2016-02-25 15:53:31 +1030

    fix eslint

--- packages/eslint-config-jemsoft/rules/variables.js
+++ packages/eslint-config-jemsoft/rules/variables.js
@@ -1,5 +1,5 @@
 module.exports = {
-  'rules': {
+  rules: {
     // enforce or disallow variable initializations at definition
     'init-declarations': 0,
     // disallow the catch clause parameter name being the same as a variable in the outer scope
@@ -11,7 +11,7 @@ module.exports = {
     // disallow shadowing of names such as arguments
     'no-shadow-restricted-names': 2,
     // disallow declaration of variables already declared in the outer scope
-    'no-shadow': [2, {'allow': ['err']},
+    'no-shadow': [2, {'allow': ['err']}],
     // disallow use of undefined when initializing variables
     'no-undef-init': 0,
     // disallow use of undeclared variables unless mentioned in a /*global */ block
